bcbee1c0e9c2d26a74ebe0b436ddb5ed60b27843
Maxwell L <mleuthner2002@gmail.com>
2025-01-30 14:44:05 -0600

    All seasons plotted

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,14 +6,22 @@ d3.json(url).then(function(data) {
     var scoresData=[];
 
     for(let i = 0; i < gamesData.length; i++){
-        if(gamesData[i].scoreHome > gamesData[i].scoreAway){
-            winningScore = gamesData[i].scoreHome
-        }else{
-            winningScore = gamesData[i].scoreAway
-        }
+        // if(gamesData[i].scoreHome > gamesData[i].scoreAway){
+        //     winningScore = gamesData[i].scoreHome
+        // }else{
+        //     winningScore = gamesData[i].scoreAway
+        // }
 
-        scoresData[i] = {score: winningScore,
-                         season: gamesData[i].scheduleSeason}
+        // scoresData[i] = {score: winningScore,
+        //                  season: gamesData[i].scheduleSeason}
+
+        if(gamesData[i].scoreHome > gamesData[i].scoreAway){
+            scoresData[i] = {score: (gamesData[i].scoreHome-gamesData[i].scoreAway),
+                season: gamesData[i].scheduleSeason}
+        }else{
+            scoresData[i] = {score: (gamesData[i].scoreAway-gamesData[i].scoreHome),
+                season: gamesData[i].scheduleSeason}
+        }
     }
     currentSeason = scoresData[0].season;
 
@@ -24,21 +32,17 @@ d3.json(url).then(function(data) {
             y0.push(scoresData[i].score)
         }else{
             let ref = y0;
-            let trace1 = {
+            let trace = {
+                name: currentSeason,
                 y: ref,
                 type: 'box'
             };
-            data.push(trace1)
+            data.push(trace)
             y0 = [];
             currentSeason = scoresData[i].season;
-            
+
         }
     }
-
-    
-
-    
-
     Plotly.newPlot('my_dataviz', data);
     console.log(scoresData);
   });
