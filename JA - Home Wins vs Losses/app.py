f1438b2388422a744539374882d5f2356e783d0f
ander@Jenna <andersonjenna01@gmail.com>
2025-02-03 19:40:41 -0600

    saving project files

---
+++ JA - Home Wins vs Losses/app.py
@@ -0,0 +1,73 @@
+# Importing the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+import pandas as pd
+import sqlite3
+import os
+from flask_cors import CORS
+
+
+
+# Apply CORS to your app
+
+# Load Data
+csv_path = 'Resources/spreadspoke_scores.xlsx'
+csv_path2 = 'Resources/nfl_teams.csv'
+
+#Edit Dataframes
+
+games = pd.read_excel(csv_path)
+games['scheduleDate'] = pd.to_datetime(games['scheduleDate'])
+games = games[games['scheduleDate'] >= '2002-06-01' ]
+
+games.fillna("NA", inplace=True)
+
+
+teams = pd.read_csv(csv_path2)
+teams.fillna("NA", inplace=True)
+
+print(teams.head())
+print(games.head())
+
+
+#Flask
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/games"
+        f"/teams"
+    )
+    
+
+
+@app.route("/games")
+def gamesAPI():
+
+    return jsonify(games.to_dict(orient='records'))
+
+
+@app.route("/teams")
+def teamsAPI():
+   
+
+    return jsonify(teams.to_dict(orient='records'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ JA - Home Wins vs Losses/static/js/index.js
@@ -0,0 +1,84 @@
+let gamesapi = 'http://127.0.0.1:5000/games';
+let teamsapi = 'http://127.0.0.1:5000/teams';
+
+d3.json(teamsapi).then(teamData => {
+  console.log("Teams Data:", teamData);
+
+  let teamSelect = document.getElementById('teamSelect');
+  let teams = Object.keys(teamData);
+
+  for (let i = 0; i < teams.length; i++) {
+    let team = teamData[i].team_name;
+    let option = document.createElement('option');
+    option.value = team;
+    option.textContent = team;
+    teamSelect.appendChild(option);
+  }
+
+  let currentTeam = teams[0];
+  updateChart(currentTeam);
+
+  teamSelect.addEventListener('change', function() {
+    currentTeam = this.value;
+    updateChart(currentTeam);
+    console.log(currentTeam)
+  });
+});
+
+let ctx = document.getElementById('lineChart').getContext('2d');
+let lineChart;
+
+function updateChart(TeamName) {
+  d3.json(gamesapi).then(gameData => {
+    console.log("Games Data:", gameData);
+
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    for (let i = 0; i < gameData.length; i++) {
+      let game = gameData[i];
+
+      if (game.teamHome === TeamName) {
+        if (game.homeResult === 'WIN') { wins++ }
+        if (game.homeResult === 'TIE') { ties++ }
+        if (game.homeResult === 'LOSS') { losses++ }
+      }
+    }
+
+    if (lineChart) {
+      lineChart.destroy();
+    }
+
+    lineChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Results'],
+          datasets: [
+            {
+              label: 'Wins',
+              data: [wins],
+              backgroundColor: 'rgba(30, 196, 21, 0.99)'
+            },
+            {
+              label: 'Losses',
+              data: [losses],
+              backgroundColor: 'rgba(214, 7, 7, 0.66)'
+            },
+            {
+                label: 'Ties',
+                data: [ties],
+                backgroundColor: 'rgba(231, 238, 38, 0.66)'
+              }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    });
+  }
